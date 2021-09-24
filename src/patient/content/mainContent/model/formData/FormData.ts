d01eb2de5da1edf50cbfb7caa6e32aa269e36744
Arthur Picht <picht@arthurpicht.de>
2021-09-24 11:39:23 +0200

    entity FormData extended, test

--- src/patient/content/mainContent/model/formData/FormData.ts
+++ src/patient/content/mainContent/model/formData/FormData.ts
@@ -1,12 +1,29 @@
+export interface Exercise {
+    sent: boolean;
+    lastModifiedTimestamp: string;
+    seenByTherapist: boolean;
+    seenByTherapistTimestamp: string;
+}
+
 export interface FormElementDataList {
     formElementType: string;    // input | radio
     formElementId: string;
     formElementValue: string;
 }
 
+export interface Feedback {
+    text: string;
+    createdTimestamp: string;
+    therapistId: string;
+    seenByPatient: boolean;
+    seenByPatientTimestamp: string;
+}
+
 export interface FormData {
     userId: string;
     contentId: string;
     editable: boolean;
+    exercise: Exercise;
     formElementDataList: FormElementDataList[];
+    feedback: Feedback;
 }
---
+++ tests/entityTest/FormData.test.ts
@@ -0,0 +1,98 @@
+import {it} from "mocha";
+import {assert} from "chai";
+import {FormData} from "../../src/patient/content/mainContent/model/formData/FormData";
+
+describe("FormData", () => {
+
+    it("simple", () => {
+        const formData: FormData
+            = JSON.parse("{\n" +
+            "  \"userId\": \"userId\",\n" +
+            "  \"contentId\": \"contentId\",\n" +
+            "  \"editable\": true,\n" +
+            "  \"formElementDataList\": [\n" +
+            "    {\n" +
+            "      \"formElementId\": \"elementId\",\n" +
+            "      \"formElementType\": \"elementType\",\n" +
+            "      \"formElementValue\": \"elementValue\"\n" +
+            "    }\n" +
+            "  ]\n" +
+            "}");
+
+        assert.equal(formData.userId, "userId", "userId");
+        assert.equal(formData.contentId, "contentId", "contentId")
+        assert.isTrue(formData.editable, "editable");
+        assert.isNotOk(formData.exercise);
+        assert.isOk(formData.formElementDataList);
+        assert.equal(formData.formElementDataList.length, 1);
+        assert.equal(formData.formElementDataList[0].formElementId, "elementId");
+        assert.equal(formData.formElementDataList[0].formElementType, "elementType");
+        assert.equal(formData.formElementDataList[0].formElementValue, "elementValue");
+
+    });
+
+    it("exercise", () => {
+        const formData: FormData
+            = JSON.parse("{\n" +
+            "  \"userId\": \"userId\",\n" +
+            "  \"contentId\": \"contentId\",\n" +
+            "  \"editable\": true,\n" +
+            "  \"exercise\": {\n" +
+            "    \"sent\": true,\n" +
+            "    \"lastModifiedTimestamp\": \"lastModifiedTimestamp\",\n" +
+            "    \"seenByTherapist\": true,\n" +
+            "    \"seenByTherapistTimestamp\": \"seenByTherapistTimestamp\"\n" +
+            "  },\n" +
+            "  \"formElementDataList\": [\n" +
+            "    {\n" +
+            "      \"formElementId\": \"elementId\",\n" +
+            "      \"formElementType\": \"elementType\",\n" +
+            "      \"formElementValue\": \"elementValue\"\n" +
+            "    }\n" +
+            "  ]\n" +
+            "}");
+
+        assert.isOk(formData.exercise);
+        assert.isTrue(formData.exercise.sent);
+        assert.equal(formData.exercise.lastModifiedTimestamp, "lastModifiedTimestamp");
+        assert.isTrue(formData.exercise.seenByTherapist);
+        assert.equal(formData.exercise.seenByTherapistTimestamp, "seenByTherapistTimestamp");
+    });
+
+    it("feedback", () => {
+        const formData: FormData
+            = JSON.parse("{\n" +
+            "  \"userId\": \"userId\",\n" +
+            "  \"contentId\": \"contentId\",\n" +
+            "  \"editable\": true,\n" +
+            "  \"exercise\": {\n" +
+            "    \"sent\": true,\n" +
+            "    \"lastModifiedTimestamp\": \"lastModifiedTimestamp\",\n" +
+            "    \"seenByTherapist\": true,\n" +
+            "    \"seenByTherapistTimestamp\": \"seenByTherapistTimestamp\"\n" +
+            "  },\n" +
+            "  \"formElementDataList\": [\n" +
+            "    {\n" +
+            "      \"formElementId\": \"elementId\",\n" +
+            "      \"formElementType\": \"elementType\",\n" +
+            "      \"formElementValue\": \"elementValue\"\n" +
+            "    }\n" +
+            "  ],\n" +
+            "  \"feedback\": {\n" +
+            "    \"text\": \"text\",\n" +
+            "    \"createdTimestamp\": \"createdTimestamp\",\n" +
+            "    \"therapistId\": \"therapistId\",\n" +
+            "    \"seenByPatient\": true,\n" +
+            "    \"seenByPatientTimestamp\": \"seenByPatientTimestamp\"\n" +
+            "  }\n" +
+            "}");
+
+        assert.isOk(formData.feedback);
+        assert.equal(formData.feedback.text, "text");
+        assert.equal(formData.feedback.createdTimestamp, "createdTimestamp");
+        assert.equal(formData.feedback.therapistId, "therapistId");
+        assert.isTrue(formData.feedback.seenByPatient);
+        assert.equal(formData.feedback.seenByPatientTimestamp, "seenByPatientTimestamp");
+    });
+
+})
